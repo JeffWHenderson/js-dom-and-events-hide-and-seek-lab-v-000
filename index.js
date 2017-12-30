aed6ec7d900946884e1b4c106fcdee37a1564379
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-30 18:24:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  let deepChildren = document.querySelector('#grand-node').querySelectorAll('div')
+  return deepChildren[deepChildren.length - 1]
+}
+
+function increaseRankBy(n) {
+  let target = document.querySelectorAll('.ranked-list li')
+  
+  for(let i = 0; i < target.length; i++) {
+      target[i].innerHTML =  parseInt(target[i].innerHTML) + n
+  }
+}
