5e1f6fcca632b2aeae740ed3251d7baeda91f224
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-30 18:24:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function deepestChild() {
 
 function increaseRankBy(n) {
   let target = document.querySelectorAll('.ranked-list li')
-  return  parseInt(target.innerHTML) + n 
+  return  target.innerHTML = parseInt(target.innerHTML) + n 
   // for(let i = 0; i < target.length; i++) {
   //     target[i].innerHTML =  parseInt(target[i].innerHTML) + n
   // }
