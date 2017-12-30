8e0bb7d284952efc29cb20a5030ed48e6d2d6806
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-30 18:10:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nestedTarget() {
 
 function deepestChild() {
   let deepChildren = document.querySelector('#grand-node').querySelectorAll('div')
-  return deepChildren[1]
+  return deepChildren[3]
 }
 
 function increaseRankBy(n) {
